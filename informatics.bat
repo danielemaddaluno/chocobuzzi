7d9760df5ae32d94d304d47c2bfdfe3b85100bec
Daniele Maddaluno <daniele.maddaluno@gmail.com>
2023-11-08 20:47:43 +0100

    Added check that the bat has been run with administrative privileges

--- informatics.bat
+++ informatics.bat
@@ -5,21 +5,43 @@ rem Documentation of "choco pin": pin a package to suppress upgrades.
 rem This is especially helpful when running choco upgrade for all packages
 rem See https://docs.chocolatey.org/en-us/choco/commands/pin
 
-echo This will first install chocolatey, then other tools
+
+rem Check if the script is run with administrative privileges
+net session >nul 2>&1
+if %errorlevel% neq 0 (
+    echo The batch file has not been launched as administrator.
+    echo You have to run this batch file as administrator.
+    pause
+    exit /b 1
+) else (
+    echo Batch file is running with administrator privileges.
+    echo .
+)
+rem pause
+rem echo .
+
+
+rem Ask confirmation to install chocolatey and tools
+echo This script will first install chocolatey, then other tools.
+echo Browse https://chocolatey.org/packages for a list of available packages
 echo .
-echo Browse https://chocolatey.org/packages for packages
-echo .
-echo Ensure that your cmd.exe runs as Administrator
-echo .
-pause
-echo .
+
+set /p user_input=Do you want to continue? (Y/n):
+
+if /i "%user_input%"=="Y" (
+    echo You confirmed. Proceeding with the operations.
+) else (
+    echo You did not confirm. Exiting.
+    pause
+    exit /b 1
+)
+
 
 rem Install Chocolatey
 powershell Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1')) && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
 
-rem By default you have to press Enter multiple times
-rem to give permission to install all of the applications.
-rem Enabling allowGlobalConfirmation it is no more required.
+rem By default you have to press Enter multiple times to give permission to install all of the applications.
+rem Enabling allowGlobalConfirmation the permission/confirmation will be given automatically.
 choco feature enable -n=allowGlobalConfirmation
 rem pause
 
@@ -27,6 +49,8 @@ echo Now chocolatey should be ready and we can go ahead
 echo .
 rem pause
 
+
+rem Install the chocolatey packages listed below
 choco install arduino
 choco install eclipse
 choco install androidstudio
@@ -62,6 +86,6 @@ choco install clion-ide
 choco install pycharm
 choco install phpstorm
 
-echo To keep your system updated, run update-all.bat regularly from an administrator cmd.exe.
+echo To keep your system updated, run update_all.bat regularly from an administrator cmd.exe.
 echo .
 pause
--- update_all.bat
+++ update_all.bat
@@ -1,5 +1,38 @@
 @echo off
 
+rem Check if the script is run with administrative privileges
+net session >nul 2>&1
+if %errorlevel% neq 0 (
+    echo The batch file has not been launched as administrator.
+    echo You have to run this batch file as administrator.
+    pause
+    exit /b 1
+) else (
+    echo Batch file is running with administrator privileges.
+    echo .
+)
+rem pause
+rem echo .
+
+
+rem Ask confirmation to install chocolatey and tools
+echo This script will update all chocolatey packages installed on this host.
+echo .
+
+set /p user_input=Do you want to continue? (Y/n):
+
+if /i "%user_input%"=="Y" (
+    echo You confirmed. Proceeding with the operations.
+) else (
+    echo You did not confirm. Exiting.
+    pause
+    exit /b 1
+)
+
+
 echo Updating all packages...
 call choco upgrade all
 echo .
+echo To keep your system updated, run this script regularly from an administrator cmd.exe.
+echo .
+pause
