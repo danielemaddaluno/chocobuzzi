dc867a07c22b3b8e5375c69a56a9f93e989920ce
Daniele Maddaluno <daniele.maddaluno@gmail.com>
2023-11-30 11:20:52 +0100

    Fixed copy ini file for emu8086

--- nochoco/install_emu8086_reg.bat
+++ nochoco/install_emu8086_reg.bat
@@ -4,12 +4,12 @@ setlocal enabledelayedexpansion
 :: Set the search path to the directory where the batch file is located
 set "searchPath=%~dp0"
 set "fileFound=false"
-set "destinationPath=C:\emu8086"
+set "destinationPath=C:\emu8086\reg.ini"
 
 for %%f in ("%searchPath%emu8086_reg.ini") do (
     set "sourceFile=%%f"
     echo Found: !sourceFile!
-    copy "%sourceFile%" "%destinationPath%"
+    copy "!sourceFile!" "%destinationPath%"
     set "fileFound=true"
 )
 
